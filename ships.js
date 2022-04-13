a939debef90452f8398b2f7e934871ad483e7aaa
Benjamin Villa <ywqqqq@gmail.com>
2022-04-13 02:39:38 +0200

    final but dirty

--- ships.js
+++ ships.js
@@ -17,70 +17,155 @@ var grid1 = {
 
 var grid2 = {
 
-    destroyer1 : [20,31],
-    destroyer2 : [39,50],
-    destroyer3 : [12,23],
-    destroyer4 : [72,73],
+    destroyer1 : [33,43],
+    destroyer2 : [35,45],
+    destroyer3 : [37,47],
+    destroyer4 : [39,49],
 
-    submarine1 : [3,4,5],
-    submarine2 : [69,80,91],
-    submarine3 : [105,106,107],
+    submarine1 : [75,85,95],
+    submarine2 : [90,91,92],
+    submarine3 : [87,88,89],
 
-    battleship1 : [54,65,76,87],
-    battleship2 : [59,48,37,26],
+    battleship1 : [70,71,72,73],
+    battleship2 : [13,14,15,16],
 
-    aircraftcarrier1 : [110,111,112,113,114]
+    aircraftcarrier1 : [11,21,31,41,51]
 }
 
 var grid3 = {
 
-    destroyer1 : [6,7],
-    destroyer2 : [9,10],
-    destroyer3 : [34,45],
-    destroyer4 : [27,28],
+    destroyer1 : [0,10],
+    destroyer2 : [30,40],
+    destroyer3 : [60,70],
+    destroyer4 : [90,91],
 
-    submarine1 : [32,43,54],
-    submarine2 : [51,62,73],
-    submarine3 : [75,86,97],
+    submarine1 : [12,22,32],
+    submarine2 : [19,29,39],
+    submarine3 : [69,79,89],
 
-    battleship1 : [68,69,70,71],
-    battleship2 : [77,88,99,110],
+    battleship1 : [72,73,74,75],
+    battleship2 : [24,25,26,27],
 
-    aircraftcarrier1 : [0,1,2,3,4]
+    aircraftcarrier1 : [52,53,54,55,56]
 }
 
 var grid4 = {
 
-    destroyer1 : [12,23],
-    destroyer2 : [19,20],
-    destroyer3 : [37,48],
-    destroyer4 : [41,42],
+    destroyer1 : [16,17],
+    destroyer2 : [36,37],
+    destroyer3 : [56,57],
+    destroyer4 : [76,77],
 
-    submarine1 : [64,75,86],
-    submarine2 : [4,5,6],
-    submarine3 : [44,55,66],
+    submarine1 : [20,21,22],
+    submarine2 : [40,41,42],
+    submarine3 : [60,61,62],
 
-    battleship1 : [79,80,81,82],
-    battleship2 : [104,105,106,107],
+    battleship1 : [0,1,2,3],
+    battleship2 : [80,81,82,83],
 
-    aircraftcarrier1 : [28,39,50,61,72]
+    aircraftcarrier1 : [24,34,44,54,64]
 }
 
 var grid5 = {
 
-    destroyer1 : [10,21],
-    destroyer2 : [27,28],
-    destroyer3 : [110,111],
-    destroyer4 : [95,106],
+    destroyer1 : [23,24],
+    destroyer2 : [70,71],
+    destroyer3 : [58,68],
+    destroyer4 : [9,19],
 
-    submarine1 : [34,45,56],
-    submarine2 : [50,51,52],
-    submarine3 : [54,65,76],
+    submarine1 : [44,54,64],
+    submarine2 : [76,86,96],
+    submarine3 : [3,4,5],
 
-    battleship1 : [11,12,13,14],
-    battleship2 : [5,6,7,8],
+    battleship1 : [11,21,31,41],
+    battleship2 : [36,37,38,39],
 
-    aircraftcarrier1 : [59,70,81,92,103]
+    aircraftcarrier1 : [90,91,92,93,94]
 }
 
-var grids = [grid1,grid2,grid3,grid4,grid5];
+var grid6 = {
+
+    destroyer1 : [89,99],
+    destroyer2 : [0,1],
+    destroyer3 : [13,14],
+    destroyer4 : [32,42],
+
+    submarine1 : [71,81,91],
+    submarine2 : [7,8,9],
+    submarine3 : [28,38,48],
+
+    battleship1 : [63,73,83,93],
+    battleship2 : [67,77,87,97],
+
+    aircraftcarrier1 : [55,65,75,85,95]
+}
+
+var grid7 = {
+
+    destroyer1 : [11,12],
+    destroyer2 : [78,68],
+    destroyer3 : [70,71],
+    destroyer4 : [44,54],
+
+    submarine1 : [32,42,52],
+    submarine2 : [74,75,76],
+    submarine3 : [4,14,24],
+
+    battleship1 : [9,8,7,6],
+    battleship2 : [26,36,46,56],
+
+    aircraftcarrier1 : [93,94,95,96,97]
+}
+
+var grid8 = {
+
+    destroyer1 : [81,91],
+    destroyer2 : [22,32],
+    destroyer3 : [83,84],
+    destroyer4 : [54,64],
+
+    submarine1 : [60,61,62],
+    submarine2 : [14,24,34],
+    submarine3 : [96,97,98],
+
+    battleship1 : [16,17,18,19],
+    battleship2 : [47,57,67,77],
+
+    aircraftcarrier1 : [0,10,20,30,40]
+}
+
+var grid9 = {
+
+    destroyer1 : [80,90],
+    destroyer2 : [40,41],
+    destroyer3 : [92,93],
+    destroyer4 : [45,46],
+
+    submarine1 : [71,72,73],
+    submarine2 : [27,28,29],
+    submarine3 : [0,10,20],
+
+    battleship1 : [22,23,24,25],
+    battleship2 : [65,75,85,95],
+
+    aircraftcarrier1 : [58,68,78,88,98]
+}
+
+var grid10 = {
+
+    destroyer1 : [54,55],
+    destroyer2 : [68,78],
+    destroyer3 : [50,60],
+    destroyer4 : [80,90],
+
+    submarine1 : [13,14,15],
+    submarine2 : [31,32,33],
+    submarine3 : [52,62,72],
+
+    battleship1 : [83,84,85,86],
+    battleship2 : [9,19,29,39],
+
+    aircraftcarrier1 : [7,17,27,37,47]
+}
+
+var grids = [grid1,grid2,grid3,grid4,grid5,grid6,grid7,grid8,grid9,grid10];
